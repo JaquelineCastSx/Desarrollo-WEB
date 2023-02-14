721ac4d8086d8f8c3cd3fe9b3bd038d2df986b20
Cesáreo Castillo <cesareocastillo@MacBook-Air-de-Cesareo-2.local>
2023-02-14 08:35:16 -0600

    Practica 4

--- practica2/index.js
+++ practica2/index.js
@@ -1,3 +1,5 @@
 let resultado = document.getElementById('resultado');
+
 const asignar = (valor) => resultado.value += valor;
+
 const calcular = () => { if (resultado.value != '') resultado.value = eval(resultado.value); }
