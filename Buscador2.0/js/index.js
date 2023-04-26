3af55a272e3f80c074bf1b9defec83dbd8a77384
Cesáreo Castillo <cesareocastillo@192.168.1.9>
2023-04-25 20:53:35 -0600

    Update Buscador

--- Buscador2.0/js/index.js
+++ Buscador2.0/js/index.js
@@ -10,9 +10,9 @@ const searchArtist = () => {
   const options = {
     method: "GET",
     headers: {
-        'content-type': 'application/octet-stream',
-		'X-RapidAPI-Key': 'af4913a6eemshad72e92cf605f20p12cefdjsn8ecc0dc062fa',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+      "content-type": "application/octet-stream",
+      "X-RapidAPI-Key": "af4913a6eemshad72e92cf605f20p12cefdjsn8ecc0dc062fa",
+      "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
     },
   };
 
@@ -30,21 +30,58 @@ const searchArtist = () => {
 const searchAlbum = () => {
   const artistId = document.getElementById("artist-id").value;
   const url = `https://spotify23.p.rapidapi.com/artist_albums/?id=${artistId}&offset=0&limit=100`;
-const options = {
-	method: 'GET',
-	headers: {
-		'content-type': 'application/octet-stream',
-		'X-RapidAPI-Key': 'af4913a6eemshad72e92cf605f20p12cefdjsn8ecc0dc062fa',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-	}
-};
+  const options = {
+    method: "GET",
+    headers: {
+      "content-type": "application/octet-stream",
+      "X-RapidAPI-Key": "af4913a6eemshad72e92cf605f20p12cefdjsn8ecc0dc062fa",
+      "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+    },
+  };
   fetch(url, options)
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
-      nombreAlbum.innerHTML = response.data.artist.discography.albums.items[0].releases.items[0].name;
-      fotoAlbum.src = response.data.artist.discography.albums.items[0].releases.items[0].coverArt.sources[0].url;
-      const idAlbum = response.data.artist.discography.albums.items[0].releases.items[0].id;
-    }).fetch(idAlbum)
-    .catch((err) => console.error(err));
+      nombreAlbum.innerHTML =
+        response.data.artist.discography.albums.items[0].releases.items[0].name;
+      fotoAlbum.src =
+        response.data.artist.discography.albums.items[0].releases.items[0].coverArt.sources[0].url;
+      const idAlbum =
+        response.data.artist.discography.albums.items[0].releases.items[0].id;
+      const url = `https://spotify23.p.rapidapi.com/albums/?ids=${idAlbum}`;
+
+      const options = {
+        method: "GET",
+        headers: {
+          "content-type": "application/octet-stream",
+          "X-RapidAPI-Key":
+            "af4913a6eemshad72e92cf605f20p12cefdjsn8ecc0dc062fa",
+          "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+        },
+      };
+      fetch(url, options)
+        .then((respuesta) => respuesta.json())
+        .then((respuesta) => {
+    
+        let canciones = respuesta.albums[0].tracks.items;
+        
+        let todasLasCanciones = '';
+        canciones.forEach((cancion,index)=>{
+            let nombreCancion = cancion.name;
+            let cancionUrl = cancion.preview_url;
+            todasLasCanciones += `
+            <div class="col-12 col-md-4 text-center">
+                <p>${nombreCancion}</p>
+                <audio controls>
+                    <source src="${cancionUrl}" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+            </div>`;
+            console.log(nombreCancion);
+            console.log(cancion.preview_url);
+        });
+        listaCanciones.innerHTML = todasLasCanciones;
+        })
+        .catch((err) => console.error(err));
+    });
 };
